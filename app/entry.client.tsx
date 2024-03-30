1fffd416c5c4df3a21c93bc648f162dc0a44bd25
Daniel Dihardja <daniel@dihardja.de>
2024-03-30 16:19:53 +0100

    setup

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,15 +4,18 @@
  * For more information, see https://remix.run/file-conventions/entry.client
  */
 
+import { ThemeProvider } from "@material-tailwind/react";
 import { RemixBrowser } from "@remix-run/react";
-import { startTransition, StrictMode } from "react";
+import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
 startTransition(() => {
   hydrateRoot(
     document,
     <StrictMode>
+    <ThemeProvider>
       <RemixBrowser />
+    </ThemeProvider>
     </StrictMode>
   );
 });
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,19 @@
 import type { MetaFunction } from "@remix-run/node";
+import pkg from "@material-tailwind/react";
+const {Button} = pkg;
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Style2Image" },
+    { name: "description", content: "Lets create some artworks" },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div>
+    <h1 className="text-3xl">{"Let's create some artworks"}</h1>
+    <a href="create">Create</a>
     </div>
-  );
+  )
 }
