53b06d9caefada351f144fd85ae073b7544e41e4
Daniel Dihardja <daniel@dihardja.de>
2024-04-04 16:18:20 +0200

    update fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,18 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 import withMT from "@material-tailwind/react/utils/withMT";
 
 export default withMT({
   content: [
     "./app/**/*.{js,jsx,ts,tsx}",
     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}"],
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 } satisfies Config);
-
