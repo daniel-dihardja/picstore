8bd1224c1c58b2e96d682dbf45b6dd66543660f0
Daniel Dihardja <daniel@dihardja.de>
2024-04-04 16:17:26 +0200

    update landing page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -13,6 +13,21 @@ export default function Index() {
   return (
     <div>
       <Header></Header>
+      <div className="flex flex-col items-center min-h-screen bg-gray-100 text-gray-800">
+        <h1 className="text-4xl font-bold mb-4 pt-12">Picstore-Mocha</h1>
+        <p className="text-xl text-center mb-8 max-w-md">
+          Generate stunning AI images, enhance prompts for unparalleled
+          creativity, and transform ideas into poster artworks. Download your
+          creations in high-quality, printable formats. Where imagination meets
+          innovation—your next masterpiece awaits.
+        </p>
+        <a
+          href="/explore"
+          className=" bg-black text-white font-bold py-2 px-4 rounded"
+        >
+          Get Started
+        </a>
+      </div>
     </div>
   );
 }
