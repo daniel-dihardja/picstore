b0e740b275c6d40020dea991db6cb1da8053ef36
Daniel Dihardja <daniel@dihardja.de>
2024-04-06 17:19:36 +0200

    refactore env var validation

--- app/.server/env.ts
+++ app/.server/env.ts
@@ -1,4 +1,24 @@
-export const {
+const requiredEnvVars = [
+  "PICSTORE_ACCESS_KEY",
+  "PICSTORE_SECRET",
+  "PICSTORE_REGION",
+  "PICSTORE_BUCKET",
+  "PICSTORE_URL",
+  "BASETEN_API_KEY",
+  "MODEL_ID",
+];
+
+const envValues = {};
+
+requiredEnvVars.forEach((varName) => {
+  const envValue = process.env[varName];
+  if (!envValue) {
+    throw new Error(`${varName} must be set.`);
+  }
+  envValues[varName] = envValue;
+});
+
+const {
   PICSTORE_ACCESS_KEY,
   PICSTORE_SECRET,
   PICSTORE_REGION,
@@ -6,34 +26,7 @@ export const {
   PICSTORE_URL,
   BASETEN_API_KEY,
   MODEL_ID,
-} = process.env;
-
-if (!PICSTORE_ACCESS_KEY) {
-  throw new Error(`PICSTORE_ACCESS_KEY must be set.`);
-}
-
-if (!PICSTORE_SECRET) {
-  throw new Error(`PICSTORE_SECRET must be set.`);
-}
-
-if (!PICSTORE_REGION) {
-  throw new Error(`PICSTORE_REGION must be set.`);
-}
-
-if (!PICSTORE_BUCKET) {
-  throw new Error(`PICSTORE_BUCKET must be set.`);
-}
-
-if (!PICSTORE_URL) {
-  throw new Error(`PICSTORE_URL url must be set.`);
-}
-
-if (!BASETEN_API_KEY) {
-  throw new Error(`BASETEN_API_KEY must be set.`);
-}
-if (!MODEL_ID) {
-  throw new Error(`MODEL_ID api key must be set.`);
-}
+} = envValues;
 
 export const env = {
   PICSTORE_ACCESS_KEY,
