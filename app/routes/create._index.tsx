4a546c5bf21af9cffad7e0d814a56f2fcd96e1ee
Daniel Dihardja <daniel@dihardja.de>
2024-04-07 07:30:21 +0200

    cleanup

--- app/routes/create._index.tsx
+++ app/routes/create._index.tsx
@@ -1,3 +1,4 @@
+// Import statements for necessary Remix and React hooks, components, and utilities.
 import {
   ActionFunctionArgs,
   LoaderFunctionArgs,
@@ -15,10 +16,9 @@ import { Header } from "~/components/Header";
 import MT from "@material-tailwind/react";
 const { Button, Card, CardBody } = MT;
 
+// Import server-side utilities for handling prompts, S3 image listings, and workflows.
 import { WorkflowValues, queuePrompt } from "~/.server/prompt-submiter";
-
 import { listImages } from "~/.server/s3-listImages";
-
 import { nanoid } from "nanoid";
 import { Pic } from "~/components/Pic";
 import { UploadPanel } from "~/components/UploadPanel";
@@ -28,13 +28,14 @@ import { PromptPanel } from "~/components/PromptPanel";
 import { env } from "~/.server/env";
 import { uploadStreamToS3 } from "~/.server/s3-upload";
 
+// Loader function: prepares data needed for the component to render.
 export async function loader({ request }: LoaderFunctionArgs) {
   const { searchParams } = new URL(request.url);
   const workflowName = searchParams.get("m");
   const files = await listImages();
   const images = files
     ?.filter((e) => e.Key?.includes(".png"))
-    .map((e) => `${process.env.PICSTORE_URL}/${e.Key}`)
+    .map((e) => `${env.PICSTORE_URL}/${e.Key}`)
     .reverse()
     .slice(0, 6);
 
@@ -43,14 +44,15 @@ export async function loader({ request }: LoaderFunctionArgs) {
   return json({ clientId, workflowName, images });
 }
 
+// Handler for generating images based on prompts.
 async function generate(request: Request) {
   if (env.ENABLE_GENERATE === "false") {
     return json({ generatedImage: "dino.jpeg" });
   }
+
   const formData = await request.formData();
   const prompt = formData.get("prompt") as string;
   const inputImage = formData.get("inputImage") as string;
-
   const workflowName = new URL(request.url).searchParams.get("m");
 
   const config: WorkflowValues = {
@@ -60,11 +62,8 @@ async function generate(request: Request) {
   };
 
   const res = await queuePrompt(workflowName as string, config);
-
   const outputImages = await listImages();
-
   const buffer = res?.imgBuffer;
-
   const folder = "output";
   const key = `image_${outputImages.length.toString().padStart(5, "0")}.png`;
   const generatedImage = await uploadStreamToS3(
@@ -74,22 +73,20 @@ async function generate(request: Request) {
     folder
   );
 
-  return json({
-    generatedImage,
-  });
+  return json({ generatedImage });
 }
 
+// Handler for uploading images.
 async function upload(request: Request) {
   const formData = await unstable_parseMultipartFormData(
     request,
     s3UploaderHandler
   );
-
   const inputImage = formData.get("file")?.toString();
-
   return json({ inputImage });
 }
 
+// Main action handler for the component, routing based on the action type.
 export async function action({ request }: ActionFunctionArgs) {
   const url = new URL(request.url);
   const type = url.searchParams.get("type") as string;
@@ -98,13 +95,13 @@ export async function action({ request }: ActionFunctionArgs) {
     case "generate":
       return generate(request);
     case "upload":
-      // eslint-disable-next-line no-case-declarations
       return upload(request);
     default:
       return json({});
   }
 }
 
+// Main component function, renders the UI for creating and managing uploads and generations.
 export default function Create() {
   const loaderData = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
@@ -116,6 +113,7 @@ export default function Create() {
 
   const navigate = useNavigate();
 
+  // Effect hook for handling loader and action data.
   useEffect(() => {
     if (loaderData) {
       setImages(loaderData.images as string[]);
@@ -129,10 +127,11 @@ export default function Create() {
         setIsGenerating(false);
       }
     }
-  }, [loaderData]);
+  }, [loaderData, actionData]);
 
   const actionUrl = `/create?clientId=${loaderData.clientId}&m=${loaderData.workflowName}`;
 
+  // Component render function.
   return (
     <div>
       <Header></Header>
@@ -141,9 +140,7 @@ export default function Create() {
           <Button
             variant="text"
             className="mb-4 rounded-full"
-            onClick={() => {
-              navigate("/explore");
-            }}
+            onClick={() => navigate("/explore")}
           >
             Back
           </Button>
@@ -167,9 +164,7 @@ export default function Create() {
           <Form
             action={`${actionUrl}&type=generate`}
             method="POST"
-            onSubmit={() => {
-              setIsGenerating(true);
-            }}
+            onSubmit={() => setIsGenerating(true)}
           >
             <input type="hidden" name="prompt" value={prompt}></input>
             <input type="hidden" name="inputImage" value={inputImage}></input>
@@ -186,8 +181,6 @@ export default function Create() {
         </div>
 
         <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-          {/* {isGenerating ? <PicProgress></PicProgress> : null} */}
-
           {images.map((imageUrl, index) => (
             <Pic key={index} url={imageUrl}></Pic>
           ))}
