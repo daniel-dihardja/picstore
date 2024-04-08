a412845e93aa0ac823e1c068605ddb0f02afd61c
Daniel Dihardja <daniel@dihardja.de>
2024-04-08 03:30:09 +0200

    refactore

--- app/services/env.server.ts
+++ app/services/env.server.ts
@@ -11,44 +11,24 @@ const requiredEnvVars = [
   "GOOGLE_CLIENT_ID",
   "GOOGLE_CLIENT_SECRET",
   "GOOGLE_CALLBACK_URL",
+  "MONGO_DB_URI",
 ];
 
-const envValues = {};
+// Function to validate and extract environment variables
+function validateAndExtractEnvVars(varNames) {
+  const envVars = {};
+  varNames.forEach((varName) => {
+    const envValue = process.env[varName];
+    if (envValue === undefined) {
+      throw new Error(`${varName} must be set.`);
+    }
+    envVars[varName] = envValue;
+  });
+  return envVars;
+}
 
-requiredEnvVars.forEach((varName) => {
-  const envValue = process.env[varName];
-  if (!envValue) {
-    throw new Error(`${varName} must be set.`);
-  }
-  envValues[varName] = envValue;
-});
+// Extract and validate environment variables
+const env = validateAndExtractEnvVars(requiredEnvVars);
 
-const {
-  PICSTORE_ACCESS_KEY,
-  PICSTORE_SECRET,
-  PICSTORE_REGION,
-  PICSTORE_BUCKET,
-  PICSTORE_URL,
-  BASETEN_API_KEY,
-  MODEL_ID,
-  ENABLE_GENERATE,
-  COMFY_PROCESSING_QUEUE,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL,
-} = envValues;
-
-export const env = {
-  PICSTORE_ACCESS_KEY,
-  PICSTORE_SECRET,
-  PICSTORE_REGION,
-  PICSTORE_BUCKET,
-  PICSTORE_URL,
-  BASETEN_API_KEY,
-  MODEL_ID,
-  ENABLE_GENERATE,
-  COMFY_PROCESSING_QUEUE,
-  GOOGLE_CLIENT_ID,
-  GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL,
-};
+// Now `env` contains all the validated environment variables
+export { env };
